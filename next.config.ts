a4f6a25514ae8a84e9f18375f7c03232bc5c2502
ayush <124794246+ayushzjjjjzk@users.noreply.github.com>
2026-03-30 09:03:07 +0530

    adding next project

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  turbopack: {
+    root: "E:/port-folio", // your actual project folder
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
